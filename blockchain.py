c36aff803a68e7e310dfa430d43ae11594a87b73
Phillip Lundin <phillip.linnemann.lundin@gmail.com>
2021-11-21 21:12:20 +0100

    Added hashes

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,12 @@
 import json
 from time import time
+import hashlib
+import json
 
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
-        self.current_trans = []
+        self.chain = [] # current chain of transactions (Verified)
+        self.current_trans = [] # Pending transactions that need to be mined (Unverified)
 
         # Genesis block is the first block initiated in the chain
         self.new_block(previous_hash=1, proof=100)
@@ -15,7 +17,7 @@ class Blockchain(object):
             'timestamp': time(),
             'transactions': self.current_trans,
             'proof': proof,
-            #'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
         # What is this
@@ -36,4 +38,6 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    
+    def hash(self, block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest() 
