09c47889ae71e856ae8f09583659fb54b0860122
Phillip Lundin <phillip.linnemann.lundin@gmail.com>
2021-11-21 21:58:02 +0100

    Finished blockchain logicstics

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # blockchain
 
+Phillip Manas Blockchain
+
 Taking a lot of inspiration from this link
 [hackernoon on blockchain](https://hackernoon.com/learn-blockchains-by-building-one-117428612f46)
--- blockchain.py
+++ blockchain.py
@@ -41,3 +41,24 @@ class Blockchain(object):
     def hash(self, block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest() 
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            #print( "Number : {} hash: {}".format(proof, hashlib.sha256("{}{}".format(last_proof, proof).encode()).hexdigest()) )
+            proof += 1
+
+        return proof
+
+    def valid_proof(self, last_proof, proof):
+        guess = "{}{}".format(last_proof, proof).encode()
+        hashGuess = hashlib.sha256(guess).hexdigest()
+        return hashGuess[:4] == "0000"
+
+
+blockchain = Blockchain()
+blockchain.new_trans("phillip", "manas", 1000)
+
+#print(hashlib.sha256("10035293".encode()).hexdigest())
+
+#print(blockchain.proof_of_work(100))
