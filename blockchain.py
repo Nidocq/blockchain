741cc0f705df0d371f265afd0e60c764368c76f8
Phillip Lundin <phillip.linnemann.lundin@gmail.com>
2021-11-21 22:05:00 +0100

    Added comments for better readabilty

--- blockchain.py
+++ blockchain.py
@@ -11,6 +11,7 @@ class Blockchain(object):
         # Genesis block is the first block initiated in the chain
         self.new_block(previous_hash=1, proof=100)
 
+    # Make a new block with custom proof and hash from previous block
     def new_block(self, proof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
@@ -26,6 +27,8 @@ class Blockchain(object):
         self.chain.append(block)
         return block
 
+
+    # Add a new transation to the current transaction
     def new_trans(self, sender, receiver, amount):
         self.current_trans.append({
             "sender": sender,
@@ -35,13 +38,16 @@ class Blockchain(object):
 
         return self.last_block()
 
+    # Get the last block in the current chain
     def last_block(self):
         return self.chain[-1]
 
+    # hash a block
     def hash(self, block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest() 
 
+    # Proof that this block is proofed
     def proof_of_work(self, last_proof):
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
@@ -50,6 +56,7 @@ class Blockchain(object):
 
         return proof
 
+    # Helper function that
     def valid_proof(self, last_proof, proof):
         guess = "{}{}".format(last_proof, proof).encode()
         hashGuess = hashlib.sha256(guess).hexdigest()
@@ -57,8 +64,6 @@ class Blockchain(object):
 
 
 blockchain = Blockchain()
-blockchain.new_trans("phillip", "manas", 1000)
-
+#blockchain.new_trans("phillip", "manas", 1000)
 #print(hashlib.sha256("10035293".encode()).hexdigest())
-
 #print(blockchain.proof_of_work(100))
